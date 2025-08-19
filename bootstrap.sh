6ac94be58780b0cbf182a87134a8fd1a014042b9
Fernando Merlo <fdomerlo@gmail.com>
2025-08-19 15:36:04 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Clonando el repositorio de configuración de la workstation..."
 git clone https://github.com/ctrbts/setup-dev-env.git
-cd /setup-dev-env
+cd ~/setup-dev-env
 bash post_install.sh --all
